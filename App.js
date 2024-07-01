b9c19778daf68a5ccc9c316c5ee9f4a2220468dd
navjotsingh2608 <navjot@argenelec.com>
2024-06-30 23:15:32 -0400

    added landing page for the app

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, Pressable, StyleSheet } from "react-native";
+import products from "./src/data/products";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Here is the app!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <Pressable
+          onPress={() => {
+            navigation.navigate("Product Details", { id: item._id });
+          }}
+          style={styles.itemContainer}
+        >
+          <Image source={{ uri: item.image }} style={styles.image} />
+        </Pressable>
+      )}
+      numColumns={2}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  itemContainer: {
+    width: "50%",
+    padding: 1,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
   },
 });
